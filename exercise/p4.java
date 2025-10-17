65a5e23ab165ad9637de60cc7585e61089ff8960
Alphaarx <arpansahoo413@gmail.com>
2025-10-18 00:24:31 +0530

    day8 part1

--- exercise/p4.java
+++ exercise/p4.java
@@ -2,22 +2,45 @@ import java.util.Scanner;
 
 public class p4 {
 
-public static void  decimalToBinary() {
-    Scanner sc= new Scanner(System.in);
-    System.out.println("enter the decimal value: ");
-    int decimal=sc.nextInt();
-    String binary="";
-    for(int i=1;i<=decimal;i++){
-    if(decimal>=i){
-        binary+="1";
-        decimal-=i;
-    }
-    else{
-        binary+="0";
-    }
-}
-System.out.println(binary);
-}
+    public static void  decimalToBinary() {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the decimal value: ");
+        int decimal=sc.nextInt();
+        String binary="";
+        for(int i = 31; i >= 0; i--){
+            int bitMask=1<<i;
+            if((bitMask&decimal)==0){
+                    binary+="0";
+                    }
+                    else{
+                        binary+="1";
+                    }
+        }
+        binary=binary.replaceAll("^0+(?!$)", ""); // to remove leading zeros
+        System.out.println("Binary:" + binary);
+    } 
+
+    public static void binaryToDecimal() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the binary value: ");
+        String binary = sc.next();
+        int decimal = 0;
+        for(int i =0; i<binary.length(); i++){
+            char bit = binary.charAt(i);
+            // int bitMasking=1<<i;
+           if (bit == '1') {
+                decimal = (decimal << 1) | 1; // left shift and set last bit
+            } else if (bit == '0') {
+                decimal = decimal << 1; // left shift, last bit is 0
+            } else {
+                System.out.println("Invalid binary number!");
+                return;
+            }
+       
+        }
+            System.out.println("Decimal equivalent: " + decimal);
+
+    }    
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
@@ -33,6 +56,8 @@ System.out.println(binary);
 
         //decimal to binary
         decimalToBinary();
+        //binary to decimal
+        binaryToDecimal();
 
     }
 
